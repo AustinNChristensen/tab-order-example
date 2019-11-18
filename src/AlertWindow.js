90f2b032e6fcefa74ad1d56a7d54b42f4c77e3c4
Mario Galeno <mgaleno@solstice.com>
2019-11-17 21:57:02 -0600

    Add bad tab index and roles to InaccessibleForm

--- src/AlertWindow.js
+++ src/AlertWindow.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
-export const AlertWindow = ({ message }) => {
+export const AlertWindow = ({ message, accessible = true }) => {
+    let role;
+    if (accessible) {
+        role = 'alert';
+    }
     return (
-        <div role='alert' className='alertWindow'>
+        <div role={role} className='alertWindow'>
             <p>{message}</p>
         </div> 
     );
