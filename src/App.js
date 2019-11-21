dd912d416a7b7957b521627270bf557528b9127f
austinchristensen <austin.christensen37@gmail.com>
2019-11-21 11:41:55 -0600

    set inaccessible as default view

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { InaccessibleForm } from './InaccessibleForm';
 import { AccessibleForm } from './AccessibleForm';
 
 const App = () => {
-  const [inaccessibleVisible, setInaccessibleVisible] = useState(false);
+  const [inaccessibleVisible, setInaccessibleVisible] = useState(true);
 
   let content;
 
